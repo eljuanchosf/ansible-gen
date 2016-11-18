2d326c9268d4299c13f38ce3c4c542966db155f6
Juan Pablo Genovese <juanpgenovese@gmail.com>
2016-11-18 16:54:51 -0300

    Add filesystem writer

--- ansibleGen/directory_tree.go
+++ ansibleGen/directory_tree.go
@@ -1,5 +1,12 @@
 package ansibleGen
 
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/afero"
+)
+
 //File represents a system file
 type File struct {
 	Name    string `yaml:"name"`
@@ -11,4 +18,27 @@ type Folder struct {
 	Name    string   `yaml:"name"`
 	Files   []File   `yaml:"files"`
 	Folders []Folder `yaml:"folders"`
+}
+
+//WriteTreeToDisk creates a directory structure based on the treeStructure parameter
+func WriteTreeToDisk(rootDir string, treeStructure Folder, baseFs *afero.Fs, dryRun bool) {
+	fs := *baseFs
+	bp := afero.NewBasePathFs(fs, rootDir)
+	newRoot := filepath.Join(rootDir, treeStructure.Name)
+	if dryRun {
+		fmt.Printf("D: %s\n", newRoot)
+	} else {
+		bp.Mkdir(treeStructure.Name, 0755)
+	}
+	bp = afero.NewBasePathFs(fs, newRoot)
+	for _, file := range treeStructure.Files {
+		if dryRun {
+			fmt.Printf("F: %s\n", filepath.Join(newRoot, file.Name))
+		} else {
+			bp.Create(file.Name)
+		}
+	}
+	for _, folder := range treeStructure.Folders {
+		WriteTreeToDisk(newRoot, folder, baseFs, dryRun)
+	}
 }
--- ansibleGen/directory_tree_test.go
+++ ansibleGen/directory_tree_test.go
@@ -1,1 +1,40 @@
 package ansibleGen
+
+import (
+	"testing"
+
+	"github.com/spf13/afero"
+)
+
+func Test_WriteTreeToDiskOnProject(t *testing.T) {
+	p := testProject()
+	baseFs := afero.NewMemMapFs()
+	WriteTreeToDisk("/tmp", p.TreeStructure, &baseFs, false)
+	if _, err := baseFs.Stat("/tmp/my_test_name/roles/crole1"); err != nil {
+		t.Errorf("Expected 'roles/crole1' to be present in filesystem structure")
+	}
+	if _, err := baseFs.Stat("/tmp/my_test_name/production"); err != nil {
+		t.Errorf("Expected '/tmp/my_test_name/production' file to be present in filesystem structure")
+	}
+}
+
+func Test_WriteTreeToDiskOnRoles(t *testing.T) {
+	r := testRole()
+	baseFs := afero.NewMemMapFs()
+	WriteTreeToDisk("/tmp", r.TreeStructure, &baseFs, false)
+	if _, err := baseFs.Stat("/tmp/my_role_name"); err != nil {
+		t.Errorf("Expected 'my_role_name' to be present in filesystem structure")
+	}
+	if _, err := baseFs.Stat("/tmp/my_role_name/tasks/main.yml"); err != nil {
+		t.Errorf("Expected '/tmp/my_role_name/tasks/main.yml' file to be present in filesystem structure")
+	}
+}
+
+func Test_WriteTreeToDiskOnDryRun(t *testing.T) {
+	r := testRole()
+	baseFs := afero.NewMemMapFs()
+	WriteTreeToDisk("/tmp", r.TreeStructure, &baseFs, true)
+	if _, err := baseFs.Stat("/tmp/my_role_name"); err == nil {
+		t.Errorf("Expected 'my_role_name' should not be present in the file structure while in Dry Run")
+	}
+}
